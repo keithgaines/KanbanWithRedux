844508bc2425e9bdd34feecae00357222316707f
keithgaines <93451842+keithgaines@users.noreply.github.com>
2023-09-11 13:49:16 -0500

    Update EllipsisMenu.jsx

--- src/components/EllipsisMenu.jsx
+++ src/components/EllipsisMenu.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 
-function EllipsisMenu({ type, setOpenEditModal, setOpenDeleteModal }) {
+function ElipsisMenu({ type, setOpenEditModal, setOpenDeleteModal }) {
   return (
     <div
       className={
-        type === "Boards" ? "absolute top-16 right-5" : "absolute top-6 right-4"
+        type === "Boards"
+          ? " absolute  top-16  right-5"
+          : " absolute  top-6  right-4"
       }
     >
-      <div className="flex justify-end items-center">
-        <div className="w-40 text-sm z-50 font-medium shadow-md shadow-[#364e7e1a] bg-white dark:bg-[#20212c] space-y-4 py-5 px-4 rounded-lg h-auto pr-12">
+      <div className=" flex justify-end items-center">
+        <div className=" w-40 text-sm z-50 font-medium shadow-md shadow-[#364e7e1a] bg-white dark:bg-[#20212c] space-y-4 py-5 px-4 rounded-lg  h-auto pr-12">
           <p
             onClick={() => {
-              setOpenEditModal(type);
+              setOpenEditModal();
             }}
-            className="cursor-pointer dark:text-gray-400 text-gray-700"
+            className=" cursor-pointer dark:text-gray-400 text-gray-700"
           >
-            Edit
+            Edit {type}
           </p>
+
           <p
-            onClick={() => setOpenDeleteModal(type)}
-            className="cursor-pointer text-red-500"
+            onClick={() => setOpenDeleteModal()}
+            className=" cursor-pointer text-red-500"
           >
-            Delete
+            Delete {type}
           </p>
         </div>
       </div>
@@ -29,4 +32,4 @@ function EllipsisMenu({ type, setOpenEditModal, setOpenDeleteModal }) {
   );
 }
 
-export default EllipsisMenu;
+export default ElipsisMenu;
